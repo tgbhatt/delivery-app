2f675432fb702b2c8ae2a668701edf843886d0ea
Tanushree <tgbhatt09@gmail.com>
2026-03-28 10:19:10 +0530

    initial project setup with Spring Boot and MySQL connection

---
+++ src/test/java/com/bt/deliveryapp/DeliveryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bt.deliveryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
